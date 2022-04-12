f53a6db2876c921b4412cd461884059242a4a177
mattdotpy <92617278+mattdotpy@users.noreply.github.com>
2022-04-12 10:12:27 -0500

    added input file and added open file functionality

--- main.py
+++ main.py
@@ -5,21 +5,32 @@ This module contains functions to
 """
 
 import sys
+import csv
 
 __author__ = 'Matthew Meyer'
 __version__ = '1.0'
 __copyright__ = "Copyright 2022.04.12, Regular Expressions"
 __github__ = "https://github.com/mattdotpy/RegEx.git"
 
+#FILENAME = "input.csv"
+
+
+#open(FILENAME, "r")
+from _csv import writer
 
 def valid_data():
+    print()
 
 
 def invalid_data():
+    print()
 
 
 def main():
-  print()
+    with open("input.txt", newline="") as file:
+        reader = csv.reader(file,  delimeter=",")
+        for row in reader:
+            print(row)
 
 
 if __name__ == '__main__':
