12bb22d6195af5f97debab8f03adda42c2f41088
mattdotpy <92617278+mattdotpy@users.noreply.github.com>
2022-04-12 09:44:08 -0500

    initial commit declared two functions

---
+++ main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+"""
+This module contains functions to
+"""
+
+import sys
+
+__author__ = 'Matthew Meyer'
+__version__ = '1.0'
+__copyright__ = "Copyright 2022.04.12, Regular Expressions"
+__github__ = "https://github.com/mattdotpy/RegEx.git"
+
+
+def valid_data():
+
+
+def invalid_data():
+
+
+def main():
+  print()
+
+
+if __name__ == '__main__':
+    main()
