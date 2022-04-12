b85775b87748f2bbc8eeac09bb4a0e8cc2c8786f
mattdotpy <92617278+mattdotpy@users.noreply.github.com>
2022-04-12 10:40:52 -0500

    Got it to write to new file

--- main.py
+++ main.py
@@ -12,25 +12,26 @@ __version__ = '1.0'
 __copyright__ = "Copyright 2022.04.12, Regular Expressions"
 __github__ = "https://github.com/mattdotpy/RegEx.git"
 
-#FILENAME = "input.csv"
-
-
-#open(FILENAME, "r")
 from _csv import writer
 
-def valid_data():
-    print()
 
+def valid_data():
+    with open("input", newline='') as file, open("invalid", "w") as invalid:
+        reader = csv.reader(file, delimiter=",")
+        for row in reader:
+            try:
+                id, name, email, phone = row
+                print(row)
+            except:
+                    writer = csv.writer(invalid)
+                    writer.writerows(row)
 
 def invalid_data():
     print()
 
 
 def main():
-    with open("input.txt", newline="") as file:
-        reader = csv.reader(file,  delimeter=",")
-        for row in reader:
-            print(row)
+    valid_data()
 
 
 if __name__ == '__main__':
