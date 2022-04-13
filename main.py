4a69040e09b2dff2022347a859628f647611842a
mifowl <mifowl00@gmail.com>
2022-04-12 20:42:04 -0500

    Fixed the format of the invalid file. Added the 'finally' for our counter.

--- main.py
+++ main.py
@@ -6,25 +6,43 @@ This module contains functions to
 
 import sys
 import csv
+import re
 
-__author__ = 'Matthew Meyer'
+__author__ = 'Matthew Meyer and Michael Fowler'
 __version__ = '1.0'
 __copyright__ = "Copyright 2022.04.12, Regular Expressions"
 __github__ = "https://github.com/mattdotpy/RegEx.git"
 
 from _csv import writer
+valid_files = 0
+invalid_files = 0
 
 
 def valid_data():
-    with open("input", newline='') as file, open("invalid", "w") as invalid:
+    global valid_files, invalid_files
+
+    with open("input", newline='') as file, open("invalid", "w", newline='') as invalid, \
+            open("valid", "w", newline='') as valid:
+
         reader = csv.reader(file, delimiter=",")
         for row in reader:
             try:
-                id, name, email, phone = row
-                print(row)
+                id, l_name, f_name, email, phone = row
+                writer = csv.writer(valid)
+                writer.writerows([row])
+                valid_files += 1
             except:
-                    writer = csv.writer(invalid)
-                    writer.writerows(row)
+                writer = csv.writer(invalid)
+                writer.writerows([row])
+                invalid_files = invalid_files + 1
+            finally:
+                print(f'This is the number of valid data: {valid_files}')
+                print()
+                print(f'This is the number of invalid data: {invalid_files}')
+
+            try:
+                id
+
 
 def invalid_data():
     print()
