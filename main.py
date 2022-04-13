032aafbc42a9fa48b62662262103cd0db863ce98
mifowl <mifowl00@gmail.com>
2022-04-12 21:22:04 -0500

    We added regular expressions for each of the variables in the row.

--- main.py
+++ main.py
@@ -26,8 +26,33 @@ def valid_data():
 
         reader = csv.reader(file, delimiter=",")
         for row in reader:
+
             try:
                 id, l_name, f_name, email, phone = row
+                pattern = '[a-zA-Z0-9]+@[a-zA-Z]+\.(edu)'
+                x = re.search(pattern, email)
+            except:
+                print(f'+E {x}')
+
+            try:
+                pattern2 = '(\d\d\d).(\d\d\d).(\d\d\d\d)'
+                re.search(pattern2, phone)
+            except:
+                print('+P')
+
+            try:
+                pattern3 = '[a-zA-Z]'
+                re.search(pattern3, l_name)
+                re.search(pattern3, f_name)
+            except:
+                print('+N')
+
+            try:
+                pattern4 = '[0-1000]'
+                re.search(pattern4, id)
+            except:
+                print('+I')
+            try:
                 writer = csv.writer(valid)
                 writer.writerows([row])
                 valid_files += 1
@@ -39,9 +64,6 @@ def valid_data():
                 print(f'This is the number of valid data: {valid_files}')
                 print()
                 print(f'This is the number of invalid data: {invalid_files}')
-
-            try:
-                id
 
 
 def invalid_data():
