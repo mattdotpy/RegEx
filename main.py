15e6221e8eb43b18ef26697ba80799f5fc62c497
mattdotpy <92617278+mattdotpy@users.noreply.github.com>
2022-04-18 20:44:30 -0500

    finished logic of project

--- main.py
+++ main.py
@@ -41,14 +41,14 @@ def validate_data():
                 if id == int(id):
                     break
             except:
-                print('else')
                 error_code += 'I'
 
             try:
-                pattern3 = '[a-zA-Z]'
+                pattern3 = '[a-zA-Z]+[,]+[a-zA-Z]'
                 n = re.search(pattern3, str(name))
                 if n == None:
                     error_code += '+N'
+                    invalid_files = invalid_files + 1
             except:
                 break
 
@@ -56,18 +56,27 @@ def validate_data():
                 pattern = '[a-zA-Z0-9]+@[a-zA-Z]+\.(edu)'
                 e = re.search(pattern, email)
                 if e == None:
-                    error_code += '+N'
+                    error_code += '+E'
+                    invalid_files = invalid_files + 1
             except:
                 break
 
             try:
-                pattern3 = '[a-zA-Z]'
-                p = re.search(pattern3, str(name))
+                pattern4 = '(\d\d\d)+\.(\d\d\d)+\.(\d\d\d\d)'
+                p = re.search(pattern4, str(phone))
                 if p == None:
-                    error_code += '+N'
+                    error_code += '+P'
+                    invalid_files = invalid_files + 1
             except:
                 break
 
+            if error_code > "":
+                invalid_writer.writerow([error_code, row])
+            else:
+                valid_writer.writerow([id, name, email, phone])
+                valid_files += 1
+        print(f'Number of invalid data:  {invalid_files}')
+        print(f'Number of valid data:  {valid_files}')
 
 
 '''
